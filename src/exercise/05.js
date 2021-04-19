b7e9d45536d29a7c3d3b25d929b131a17ea72c0d
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-04-19 00:50:29 -0300

    chore: exercises 05-06

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,46 +1,49 @@
 // useRef and useEffect: DOM interaction
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
+import * as React from 'react';
 // eslint-disable-next-line no-unused-vars
-import VanillaTilt from 'vanilla-tilt'
+import VanillaTilt from 'vanilla-tilt';
 
-function Tilt({children}) {
-  // 🐨 create a ref here with React.useRef()
+function Tilt({ children }) {
+  const tiltRef = React.useRef();
 
-  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
-  // div look fancy.
-  // 💰 like this:
-  // const tiltNode = tiltRef.current
-  // VanillaTilt.init(tiltNode, {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // })
-  //
-  // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
-  // object to your DOM node to cleanup:
-  // `return () => tiltNode.vanillaTilt.destroy()`
-  //
-  // 💰 Don't forget to specify your effect's dependencies array! In our case
-  // we know that the tilt node will never change, so make it `[]`. Ask me about
-  // this for a more in depth explanation.
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current;
 
-  // 🐨 add the `ref` prop to the `tilt-root` div here:
+    VanillaTilt.init(tiltNode, {
+      max: 25,
+      speed: 400,
+      glare: false,
+      'max-glare': 0.5,
+    });
+
+    return () => tiltNode.vanillaTilt.destroy();
+    // useEffect return fn works as a cleanup mechanic so that it doesn't
+    // kill performance every rerender (here for example by latching listeners onto
+    // a node that already exists)
+
+    // behaviour with dependencies:
+
+    // if no array of deps are present: cleanup happens every rerender
+    // if an empty array is present: cleanup happens on unmount
+    // if the array contains references: cleanup happens every time these references
+    // changes, thus running the cleanup before the side effect happens again
+  }, []);
+
   return (
-    <div className="tilt-root">
+    <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
     </div>
-  )
+  );
 }
 
 function App() {
   return (
     <Tilt>
-      <div className="totally-centered">vanilla-tilt.js</div>
+      <div className="totally-centered">tada!</div>
     </Tilt>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,38 +1,56 @@
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import * as React from 'react';
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon';
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => { /* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+import { ErrorBoundary } from 'react-error-boundary';
 
-  // 💣 remove this
-  return 'TODO'
+function PokemonInfo({ pokemonName }) {
+  const [{ status, pokemon, error }, setState] = React.useState({
+    status: pokemonName ? 'pending' : 'idle',
+    pokemon: null,
+    error: null,
+  });
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    setState({ status: 'pending' });
+    fetchPokemon(pokemonName)
+      .then(pokemon => {
+        setState({ status: 'resolved', pokemon });
+      })
+      .catch(error => {
+        setState({ status: 'rejected', error });
+      });
+  }, [pokemonName]);
+
+  switch (status) {
+    case 'idle':
+      return 'Submit a pokemon!';
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />;
+    case 'resolved':
+      return <PokemonDataView pokemon={pokemon} />;
+    case 'rejected':
+      throw error;
+    default:
+      throw new Error('Error: status not supported');
+  }
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
+  const [pokemonName, setPokemonName] = React.useState('');
 
   function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
+    setPokemonName(newPokemonName);
   }
 
   return (
@@ -40,10 +58,16 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary
+          FallbackComponent={() => <div>My custom fallback component..</div>}
+          resetKeys={[pokemonName]}
+          onReset={() => setPokemonName('')}
+        >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
